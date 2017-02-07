87e80f351d9afc7b1f06247b0d11e8ebbece0dc0
vinoth <vinoth.vinothrenga@gmail.com>
2017-02-07 07:34:34 +0530

    starting with quotes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <router-view></router-view><br>
+    <h2> - Knute Rockne</h2>
   </div>
 </template>
 
